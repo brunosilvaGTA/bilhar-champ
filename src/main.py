f8c464fe5d2d7b74f7c0b9ed9e2194208e2e2062
RodResq <rodrigoresque@yahoo.com.br>
2025-02-24 19:08:24 -0300

    updt

--- src/main.py
+++ src/main.py
@@ -15,8 +15,6 @@ def hello_world():
 def app_home():
     css_url = url_for('static', filename='css/styles.css')
     return render_template('child.html', css_url=css_url)
-
-
     
 if __name__ == '__main__':
     app.run(debug=True)
