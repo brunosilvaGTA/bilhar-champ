395c79a929dcf2307696b0b7a77cd211a00348f7
RodResq <rodrigoresque@yahoo.com.br>
2025-02-24 20:03:59 -0300

    update

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,6 @@
-
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
@@ -11,4 +8,7 @@ def hello_world():
 
 @app.route("/home")
 def app_home():
-    return render_template('exemplo.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
